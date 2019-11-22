8a6bd8517c5f03dd8fdee17609de70f819d2cc30
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-11-22 19:41:31 +0100

    a

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -75,10 +75,11 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
 			$this->redirect('Homepage:courses');
 		}
 		$course = $this->database->table("course")->where("id=?", $id)->fetch();
-		$course_guarantor = $this->database->table("user")->where("id=?", $course->id_guarantor)->fetch();
-		$course->guarantor=$course_guarantor->first_name . " " . $course_guarantor->surname;
+		
 		if($course)
 		{
+			$course_guarantor = $this->database->table("user")->where("id=?", $course->id_guarantor)->fetch();
+			$this->template->guarantor=$course_guarantor->first_name . " " . $course_guarantor->surname;
 			switch($course->type)
 			{
 				case P:$course->type="Povinný";break;
