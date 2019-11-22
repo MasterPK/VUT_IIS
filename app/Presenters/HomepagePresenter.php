ab3604485b9d00de9d47749896f9a6c8b2f07268
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-11-22 16:17:37 +0100

    a

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -11,20 +11,30 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
 	private $database;
 	public function __construct(Nette\Database\Context $database)
-    {
-        $this->database = $database;
-    }
+	{
+		$this->database = $database;
+	}
 
 
-    public function startUp()
+	public function startUp()
 	{
 		parent::startup();
 
-		$this->getUser()->isLoggedIn() ? "":$this->redirect("Login:");
+		if ($this->getUser()->isLoggedIn()) 
+		{
+			
+			$data = $this->database->table("users")
+				->where("id=?", $this->user->identity->id)
+				->fetch();
+			$this->template->rank = $data->rank;
+		} 
+		else 
+		{
+			$this->template->rank = "Neregistrovaný uživatel";
+		}
 
-		$data=$this->database->table("users")
-		->where("id=?",$this->user->identity->id)
-		->fetch();
+
+
 
 		/*$userData=new Nette\Security\Identity ($this->user->identity->ID,$this->user->identity->rank,$data);
 
@@ -36,15 +46,9 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
 				$this->user->identity->$key = $item;
 			}
 		}*/
-
 	}
 
 
-    public function renderDefault(): void
-    {
-        
-
-        
-    }
-    
+	public function renderDefault(): void
+	{ }
 }
