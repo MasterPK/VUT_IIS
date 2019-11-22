ec424b6f123279cb9b72ffb095136e09004b52ab
Paradox <paradox@pop-os.localdomain>
2019-11-22 23:23:43 +0100

    trying to shorten url

--- app/Router/RouterFactory.php
+++ app/Router/RouterFactory.php
@@ -15,8 +15,8 @@ final class RouterFactory
 	public static function createRouter(): RouteList
 	{
 		$router = new RouteList;
-		$router->addRoute('<presenter>/<action>[/<id>]', 'Homepage:default');
 		$router->addRoute('login', 'Login:login');
+		$router->addRoute('<presenter>/<action>[/<id>]', 'Homepage:default');
 		return $router;
 	}
 }
