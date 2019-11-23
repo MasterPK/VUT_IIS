c9428bb8de836dc4bf2592051c7271d1fb504501
Parradox27 <40549021+Parradox27@users.noreply.github.com>
2019-11-23 18:57:48 +0100

    added guarantor support for course management

--- app/Presenters/StudentPresenter.php
+++ app/Presenters/StudentPresenter.php
@@ -76,7 +76,17 @@ final class StudentPresenter extends Nette\Application\UI\Presenter
 
 	public function renderLector(): void
 	{
-		$data = $this->database->query("SELECT id_course, name, type, price FROM user NATURAL JOIN course_has_lecturer NATURAL JOIN course WHERE id_user = ?",  $this->user->identity->id);
+		$data = NULL;
+		switch($this->template->rank)
+		{
+			case 2:
+				$data = $this->database->query("SELECT id_course, name, type, price FROM user NATURAL JOIN course_has_lecturer NATURAL JOIN course WHERE id_user = ?",  $this->user->identity->id);
+				break;
+			case 3:
+				$data = $this->database->query("SELECT id_course, name, type, price FROM course WHERE id_guarantor = ?",  $this->user->identity->id);
+				break;
+		}
+		
 
 		if($data->getRowCount() > 0)
 		{
