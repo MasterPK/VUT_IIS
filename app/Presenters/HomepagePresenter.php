5f3a3d3d2122b911102396d6bde84a7cbef4df54
Parradox27 <40549021+Parradox27@users.noreply.github.com>
2019-11-23 21:38:52 +0100

    added button

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -57,8 +57,11 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
 	}
 
 
-	public function renderDefault(): void
-	{ }
+	public function renderDefault($id): void
+	{ 
+
+		addNotification($id);
+	}
 
 	public function renderCourses($search, $filter): void
 	{
@@ -160,24 +163,15 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
 	}
 	
 
-	protected function createComponentRegisterForm(): UI\Form
-    {
-        $form = new UI\Form;
+	
 
-        $form->addSubmit('register', 'Registrovat kurz')
-		->setHtmlAttribute('class', 'btn btn-block btn-primary');
-		
-		$form->onSuccess[] = [$this, 'addNotification'];
-        return $form;
-    }
-
-    public function addNotification(UI\Form $form): void
+    public function addNotification($id_course): void
     {
-    	$get = $this->database->query("SELECT id_notification FROM notification WHERE id_course = ? AND id_user = ?", $this->template->course->id_course, $this->user->identity->id);
+    	$get = $this->database->query("SELECT id_notification FROM notification WHERE id_course = ? AND id_user = ?", $id_course, $this->user->identity->id);
 
     	if($get->getRowCount() == 0)
     	{
-    		$data = $this->database->query("INSERT INTO notification ( id_notification, id_course, id_user) VALUES ('', ?, ?)", $this->template->course->id_course, $this->user->identity->id);
+    		$data = $this->database->query("INSERT INTO notification ( id_notification, id_course, id_user) VALUES ('', ?, ?)", $id_course, $this->user->identity->id);
     	}
     	else
     	{
