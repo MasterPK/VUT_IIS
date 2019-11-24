43fd4c59444612154cf247209749734a72aa3fee
Parradox27 <40549021+Parradox27@users.noreply.github.com>
2019-11-24 14:03:25 +0100

    Update RouterFactory.php

--- app/Router/RouterFactory.php
+++ app/Router/RouterFactory.php
@@ -16,6 +16,8 @@ final class RouterFactory
 	{
 		$router = new RouteList;
 		$router->addRoute('request/<id>', 'Request:request');
+		$router->addRoute('lector', 'Student:lector');
+		$router->addRoute('create', 'Student:create');
 		$router->addRoute('login', 'Login:login');
 		$router->addRoute('logout', 'Login:logout');
 		$router->addRoute('<presenter>/<action>[/<id>]', 'Homepage:default');
