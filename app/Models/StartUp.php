0aeb0b34624b7b468104d345182bf73f51617303
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-11-24 15:25:53 +0100

    Update StartUp.php

--- app/Models/StartUp.php
+++ app/Models/StartUp.php
@@ -33,7 +33,6 @@ class StartUp
 				}
             }
             $presenter->template->username=$presenter->user->identity->data["first_name"] . " " . $presenter->user->identity->data["surname"]; 
-            dump($presenter->template->username);
 			$presenter->template->rank=$data->rank;
 			switch($data->rank)
 			{
