5a9f330e204576b03bfa6d802177b88e985e757b
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-11-24 18:02:05 +0100

    a

--- app/Presenters/RequestPresenter.php
+++ app/Presenters/RequestPresenter.php
@@ -61,9 +61,10 @@ final class RequestPresenter extends Nette\Application\UI\Presenter
 		$requests = $this->database->query("SELECT id_user, email, first_name, surname FROM user NATURAL JOIN course_has_student NATURAL JOIN course WHERE id_guarantor = ? AND id_course = ? AND status = 1", $this->user->identity->id, $this->id_course)->fetchAll();
 
 		$form = new Form;
+		$main = $form->addContainer('checkboxes');
 		foreach($requests as $row)
 		{
-			$form->addCheckbox($row->id_user, '');
+			$main->addCheckbox($row->id_user, '');
 		}
         $form->addSubmit('submit', 'Zaregistrovat označené')
         ->setHtmlAttribute('class', 'btn btn-primary');
