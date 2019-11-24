bb1374ec551b610ebd0791ec976116011dde8db4
Parradox27 <40549021+Parradox27@users.noreply.github.com>
2019-11-24 22:11:59 +0100

    Update RequestPresenter.php

--- app/Presenters/RequestPresenter.php
+++ app/Presenters/RequestPresenter.php
@@ -87,7 +87,7 @@ final class RequestPresenter extends Nette\Application\UI\Presenter
 		{
 			$guarantor = $this->database->query("SELECT first_name, surname FROM user WHERE id_user = ?", $course->id_guarantor)->fetch();
 
-			Debugger::log($course->id_guarantor);
+			dump($course->id_guarantor);
 
 			$course->id_guarantor = $guarantor->first_name . " " . $guarantor->surname;
 			
