d8aa6e10442cc875eebd7493825057cac1746d94
Parradox27 <40549021+Parradox27@users.noreply.github.com>
2019-11-24 13:24:31 +0100

    Update HomepagePresenter.php

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -145,7 +145,7 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
 			$this->redirect('Homepage:courses');
 		}
 
-		$requests = $this->database->query("SELECT email, first_name, surname FROM user NATURAL JOIN request NATURAL JOIN course WHERE id_guarantor = ?", $this->user->identity->id);
+		$requests = $this->database->query("SELECT email, first_name, surname FROM user NATURAL JOIN request NATURAL JOIN course WHERE id_guarantor = ? AND id_course = ?", $this->user->identity->id, $id);
 		if($requests->getRowCount() > 0)
 		{
 			$this->template->requests = $requests;
