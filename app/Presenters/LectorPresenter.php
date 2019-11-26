05220896399ba7cfed4f34db82ec2757f797236e
Parradox27 <40549021+Parradox27@users.noreply.github.com>
2019-11-26 17:02:56 +0100

    added lector folder with lector latte

--- app/Presenters/LectorPresenter.php
+++ app/Presenters/LectorPresenter.php
@@ -45,4 +45,40 @@ final class LectorPresenter extends Nette\Application\UI\Presenter
 		
 	}
 	
+	public function renderLector(): void
+	{
+		$courses = array();
+		switch($this->template->rank)
+		{
+			case 5:
+			case 4:
+			case 3:
+				$data = $this->database->query("SELECT id_course, course_name, course_type, course_status FROM course WHERE id_guarantor = ?",  $this->user->identity->id);
+				if($data->getRowCount() > 0)
+				{
+					foreach($data as $course)
+					{
+						
+						array_push($courses, $course);
+					}
+				}
+			case 2:
+				$data = $this->database->query("SELECT id_course, course_name, course_type, course_status FROM user NATURAL JOIN course_has_lecturer NATURAL JOIN course WHERE id_user = ? AND course_status != 0",  $this->user->identity->id);
+				if($data->getRowCount() > 0)
+				{
+					foreach($data as $course)
+					{
+						array_push($courses, $course);
+					}
+				}
+				break;
+			default:
+				break;
+		}
+		
+		if(count($courses) > 0)
+		{
+			$this->template->courses=$courses;
+		}
+	}
 }
--- app/Presenters/StudentPresenter.php
+++ app/Presenters/StudentPresenter.php
@@ -42,41 +42,4 @@ final class StudentPresenter extends Nette\Application\UI\Presenter
 		$this->template->courses=$this->studentModel->getCoursesOfStudent($this->user->identity->id);
 		
 	}
-
-	public function renderLector(): void
-	{
-		$courses = array();
-		switch($this->template->rank)
-		{
-			case 5:
-			case 4:
-			case 3:
-				$data = $this->database->query("SELECT id_course, course_name, course_type, course_status FROM course WHERE id_guarantor = ?",  $this->user->identity->id);
-				if($data->getRowCount() > 0)
-				{
-					foreach($data as $course)
-					{
-						
-						array_push($courses, $course);
-					}
-				}
-			case 2:
-				$data = $this->database->query("SELECT id_course, course_name, course_type, course_status FROM user NATURAL JOIN course_has_lecturer NATURAL JOIN course WHERE id_user = ? AND course_status != 0",  $this->user->identity->id);
-				if($data->getRowCount() > 0)
-				{
-					foreach($data as $course)
-					{
-						array_push($courses, $course);
-					}
-				}
-				break;
-			default:
-				break;
-		}
-		
-		if(count($courses) > 0)
-		{
-			$this->template->courses=$courses;
-		}
-	}
 }
