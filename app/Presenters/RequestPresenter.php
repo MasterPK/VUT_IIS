d86b8575b75278e176c4d74d51b0fdbd1f053291
Parradox27 <40549021+Parradox27@users.noreply.github.com>
2019-11-26 21:58:34 +0100

    added private id course variable

--- app/Presenters/RequestPresenter.php
+++ app/Presenters/RequestPresenter.php
@@ -22,6 +22,8 @@ final class RequestPresenter extends Nette\Application\UI\Presenter
 
 	private $database;
 
+	private $id_course;
+
 	public function __construct(Nette\Database\Context $database)
 	{
 		$this->database = $database;
