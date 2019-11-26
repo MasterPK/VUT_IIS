bf3dffac7570b90886087b255368278bd851cc4d
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-11-26 20:26:48 +0100

    Update RequestPresenter.php

--- app/Presenters/RequestPresenter.php
+++ app/Presenters/RequestPresenter.php
@@ -161,7 +161,7 @@ final class RequestPresenter extends Nette\Application\UI\Presenter
 		if ($this->isAjax())
 		{
 			$this->template->error_notif = 2;
-            $this->sendResponse(new Nette\Utils\JsonResponse(['klic' => 'hodnota']));
+            $this->sendResponse("ahoj");
         }
 		
     	
