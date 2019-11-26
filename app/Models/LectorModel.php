4eb368045c2898cde651fb824226d81f796ea312
Martin Klobušický <martin.klobusicky@centrum.sl>
2019-11-26 21:30:47 +0100

    lektor courses

--- app/Models/LectorModel.php
+++ app/Models/LectorModel.php
@@ -8,8 +8,12 @@ use Nette\Application\UI\Form;
 
 class LectorModel
 {
-    /** @var Nette\Database\Context @inject */
-    public $database;
+    
+    private $database;
+    public function __construct(Nette\Database\Context $database)
+    {
+        $this->database = $database;
+    }
 
 	/** @var \App\Model\StudentModel @inject */
     public $studentModel;
@@ -17,5 +21,23 @@ class LectorModel
     public function getCoursesOfLector($id_lector)
     {
         return $this->studentModel->getCoursesOfStudent($id_lector);
+    }
+
+    public function getLectorCourses($id_lector)
+    {
+        $courses = array();
+        $data = $this->database->query("SELECT id_course, course_name, course_type, course_status FROM user NATURAL JOIN course_has_lecturer NATURAL JOIN course WHERE id_user = ? AND course_status != 0",  $id_lector);
+        if($data->getRowCount() > 0)
+        {
+            foreach($data as $course)
+            {
+                array_push($courses, $course);
+            }
+        }
+
+        if(count($courses) > 0)
+		{
+			return $courses;
+		}
     }
 }
