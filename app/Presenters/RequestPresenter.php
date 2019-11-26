4f8374444cb5770ff3e79bea96895d65e249a9a2
Parradox27 <40549021+Parradox27@users.noreply.github.com>
2019-11-27 00:44:58 +0100

    Update RequestPresenter.php

--- app/Presenters/RequestPresenter.php
+++ app/Presenters/RequestPresenter.php
@@ -154,7 +154,6 @@ final class RequestPresenter extends Nette\Application\UI\Presenter
 			//ak sa nejaky update nevykona, ukonci s chybou
 			if($result->getRowCount() == 0)
 			{
-				$this->handleFinish();
 				$this->sendResponse( new Nette\Application\Responses\JsonResponse( ['status' => 'error'] ) );
 				return;
 			}
@@ -163,7 +162,6 @@ final class RequestPresenter extends Nette\Application\UI\Presenter
 
 		if ($this->isAjax())
 		{
-			$this->handleFinish();
             $this->sendResponse( new Nette\Application\Responses\JsonResponse( ['status' => 'success'] ) );
         }
 		
