b45c8d36fc2dd8fde303678971cca9326cef92de
Martin Klobušický <martin.klobusicky@centrum.sl>
2019-11-26 14:54:34 +0100

    lektor setup

--- app/Presenters/LectorPresenter.php
+++ app/Presenters/LectorPresenter.php
@@ -13,12 +13,8 @@ final class StudentPresenter extends Nette\Application\UI\Presenter
 	/** @var \App\Model\StartUp @inject */
     public $startup;
 
-
-	private $database;
-	public function __construct(Nette\Database\Context $database)
-	{
-		$this->database = $database;
-	}
+	/** @var Nette\Database\Context @inject */
+	public $database;
 
 
 	public function startUp()
@@ -27,118 +23,9 @@ final class StudentPresenter extends Nette\Application\UI\Presenter
 
 		
 		$this->startup->mainStartUp($this);
-		if(!$this->startup->roleCheck($this,1))
+		if(!$this->startup->roleCheck($this,2))
 		{
 			$this->redirect("Homepage:default");
 		}
 
 	}
-
-
-	public function renderDefault(): void
-	{ }
-
-	public function renderCourses(): void
-	{
-		$data = $this->database->query("SELECT id_course, course_name, course_type, course_price FROM user NATURAL JOIN course_has_student NATURAL JOIN course WHERE id_user = ? AND student_status = 1 AND course_status != 0",  $this->user->identity->id);
-
-		if($data->getRowCount() > 0)
-		{
-			$this->template->courses=$data;
-		}
-	}
-
-	public function renderLector(): void
-	{
-		$courses = array();
-		switch($this->template->rank)
-		{
-			case 5:
-			case 4:
-			case 3:
-				$data = $this->database->query("SELECT id_course, course_name, course_type, course_status FROM course WHERE id_guarantor = ?",  $this->user->identity->id);
-				if($data->getRowCount() > 0)
-				{
-					foreach($data as $course)
-					{
-						
-						array_push($courses, $course);
-					}
-				}
-			case 2:
-				$data = $this->database->query("SELECT id_course, course_name, course_type, course_status FROM user NATURAL JOIN course_has_lecturer NATURAL JOIN course WHERE id_user = ? AND course_status != 0",  $this->user->identity->id);
-				if($data->getRowCount() > 0)
-				{
-					foreach($data as $course)
-					{
-						array_push($courses, $course);
-					}
-				}
-				break;
-			default:
-				break;
-		}
-		
-		if(count($courses) > 0)
-		{
-			$this->template->courses=$courses;
-		}
-	}
-
-	protected function createComponentCreateCourseForm(): Form
-    {
-        $form = new Form;
-
-        $form->addText('id_course', 'Zkratka kurzu')
-        ->setHtmlAttribute('class', 'form-control')
-        ->setRequired()
-        ->addRule(Form::PATTERN, 'Zadejte 3 až 5 velkých písmen!', '([A-Z]\s*){3,5}');
-
-        $form->addText('name', 'Název kurzu')
-        ->setHtmlAttribute('class', 'form-control')
-        ->setRequired()
-        ->addRule(Form::MIN_LENGTH, 'Dĺžka jména musí být 5 až 30 znaků!', 5)
-        ->addRule(Form::MAX_LENGTH, 'Dĺžka jména musí být 5 až 30 znaků!', 30);
-
-        $form->addText('description', 'Popis')
-        ->setHtmlAttribute('class', 'form-control')
-        ->setRequired()
-        ->addRule(Form::MIN_LENGTH, 'Dĺžka popisu musí být 5 až 500 znaků!', 5)
-        ->addRule(Form::MAX_LENGTH, 'Dĺžka popisu musí být 5 až 500 znaků!', 500);
-
-        $form->addSelect('type', 'Typ', [
-		    'P' => 'Povinný',
-		    'V' => 'Volitelný',
-		])
-        ->setHtmlAttribute('class', 'form-control')
-        ->setRequired();
-
-		$form->addText('price', 'Cena')
-        ->setHtmlAttribute('class', 'form-control')
-        ->setRequired()
-        ->addRule(Form::PATTERN, 'Zadejte číslo v rozmezí 0 - 1000 000 000!', '([0-9]\s*){1,10}');
-
-        $form->addSubmit('create', 'Vytvořit kurz')
-        ->setHtmlAttribute('class', 'btn btn-block btn-primary');
-        
-        $form->onSuccess[] = [$this, 'createCourseForm'];
-        return $form;
-	}
-
-	public function createCourseForm(Nette\Application\UI\Form $form): void
-    {
-    	$values = $form->getValues();
-
-    	try
-    	{
-    		$data = $this->database->query("INSERT INTO course (id_course, course_name, course_description, course_type, course_price, id_guarantor, course_status) VALUES (?, ?, ?, ?, ?, ?, 0)", $values->id_course, $values->name, $values->description, $values->type, $values->price,  $this->user->identity->id);
-
-    		$this->template->success_insert = true;
-    	}
-    	catch(Nette\Database\UniqueConstraintViolationException $e)
-    	{
-    		$this->template->error_insert=true;
-    		$this->template->error_course=$values->id_course;
-    	}
-	}
-}
