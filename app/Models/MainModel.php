e8683ffaff50c20b24f3ea7a4f9f6517b9526c25
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-11-27 20:20:13 +0100

    a

--- app/Models/MainModel.php
+++ app/Models/MainModel.php
@@ -197,4 +197,26 @@ class MainModel
 		}
     }
 
+    public function createComponentSearchCourseForm($presenter): Nette\Application\UI\Form
+    {
+        $form = new Nette\Application\UI\Form;
+
+        $form->addSelect('filter', 'Filter', [
+		    'course_name' => 'Název',
+		    'id_course' => 'Zkratka',
+		    'course_type' => 'Typ',
+		    'course_price' => 'Cena',
+		]);
+
+        $form->addText('search', 'Hledat:')
+        ->setHtmlAttribute('class', 'form-control')
+        ->setRequired();
+
+        $form->addSubmit('send', 'Hledat')
+        ->setHtmlAttribute('class', 'btn btn-block btn-primary');
+        
+        $form->onSuccess[] = [$presenter, 'searchCourseForm'];
+        return $form;
+	}
+
 }
