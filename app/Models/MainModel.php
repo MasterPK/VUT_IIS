d4e969d6084a8d7276ec43513c9fc6119e325499
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-11-27 21:51:23 +0100

    a

--- app/Models/MainModel.php
+++ app/Models/MainModel.php
@@ -196,5 +196,26 @@ class MainModel
 			return $courses;
 		}
     }
+    public function createComponentSearchCourseForm($presenter): Nette\Application\UI\Form
+    {
+        $form = new Nette\Application\UI\Form;
+
+        $form->addSelect('filter', 'Filter', [
+		    'course_name' => 'Název',
+		    'id_course' => 'Zkratka',
+		    'course_type' => 'Typ',
+		    'course_price' => 'Cena',
+		]);
+
+        $form->addText('search', 'Hledat:')
+        ->setHtmlAttribute('class', 'form-control')
+        ->setRequired();
+
+        $form->addSubmit('send', 'Hledat')
+        ->setHtmlAttribute('class', 'btn btn-block btn-primary');
+        
+        $form->onSuccess[] = [$presenter, 'searchCourseForm'];
+        return $form;
+	}
 
 }
--- app/Presenters/GarantPresenter.php
+++ app/Presenters/GarantPresenter.php
@@ -147,7 +147,7 @@ final class GarantPresenter extends Nette\Application\UI\Presenter
 
 	public function createComponentSearchCourseForm(): Nette\Application\UI\Form
     {
-        return $this->formsFactory->createComponentSearchCourseForm($this);
+        return $this->mainModel->createComponentSearchCourseForm($this);
 	}
 	
 	public function searchCourseForm(Nette\Application\UI\Form $form): void
