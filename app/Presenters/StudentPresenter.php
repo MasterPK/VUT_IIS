1f8d7cbf71088a72c7dd7097cc08362c22bc7347
Martin Klobušický <martin.klobusicky@centrum.sl>
2019-11-27 14:50:46 +0100

    student data

--- app/Presenters/StudentPresenter.php
+++ app/Presenters/StudentPresenter.php
@@ -53,7 +53,8 @@ final class StudentPresenter extends Nette\Application\UI\Presenter
 
 	public function renderMyCourseDetails($id_course): void
 	{
-		
+		$data = $this->database->table("course_has_task")->where("id_course=?", $id_course)->fetchAll();
+		$this->template->courses = $data;
 	}
 
 	public function createComponentRegisterForm()
