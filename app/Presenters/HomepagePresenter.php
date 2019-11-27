29a5fca74ec31d580fe858372618c5059cd5975f
Parradox27 <40549021+Parradox27@users.noreply.github.com>
2019-11-27 16:28:14 +0100

    Update HomepagePresenter.php

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -9,7 +9,7 @@ use Nette;
 use Nette\Application\UI;
 
 
-class HomepagePresenter implements Interfaces\HomepagePresenterI
+class HomepagePresenter extends Interfaces\HomepagePresenterI
 {
 	/** @var \App\Model\StartUp @inject */
 	public $startup;
