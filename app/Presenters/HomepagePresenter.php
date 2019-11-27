e2a888d7193f8cf9938a7ea3de73b790f6f08575
Parradox27 <40549021+Parradox27@users.noreply.github.com>
2019-11-27 17:28:19 +0100

    added basepresenter

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -9,75 +9,8 @@ use Nette;
 use Nette\Application\UI;
 
 
-class HomepagePresenter extends Nette\Application\UI\Presenter
+class HomepagePresenter extends BasePresenter
 {
-	/** @var \App\Model\StartUp @inject */
-	public $startup;
-	
-	/** @var \App\Model\VisitorModel @inject */
-	public $visitorModel;
-	
-	/** @var \App\Model\MainModel @inject */
-	public $mainModel;
-	
-	/** @var \App\ComponentFactories\FormsFactory @inject */
-    public $formsFactory;
-
-	private $database;
-	public function __construct(Nette\Database\Context $database)
-	{
-		$this->database = $database;
-	}
-
-	private $current_course_id;
-
-
-	public function startUp()
-	{
-		parent::startup();
-
-		$this->startup->mainStartUp($this);
-		
-	}
-
-
-	public function renderDefault(): void
-	{ 
-
-	}
-
-	public function renderCourses($search, $filter): void
-	{
-		if($search)
-		{
-			$this->template->courses=$this->mainModel->getAllCoursesByFilter($filter, $search);
-		}
-		else
-		{
-			//zobraz vsetky schvalene kurzy
-			$this->template->courses=$this->mainModel->getAllApprovedCourses();
-		}
-	}
-
-	public function renderShowcourse($id): void
-	{
-		$this->visitorModel->renderShowcourse($this,$id);
-		
-	}
-
-	public function createComponentSearchCourseForm(): Nette\Application\UI\Form
-    {
-        return $this->formsFactory->createComponentSearchCourseForm($this);
-	}
-	
-	public function searchCourseForm(Nette\Application\UI\Form $form): void
-    {
-    	$values = $form->getValues();
-    	$this->redirect("Homepage:courses", $values->search, $values->filter);
-	}
-	
-
-
 	public function handleOpen($id)
     {
     	$get = $this->database->query("UPDATE course SET course_status = 2 WHERE id_course = ?", $id);
