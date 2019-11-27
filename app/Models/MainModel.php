2cee71ae1347f6ef80e441590849539be71ecd7f
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-11-27 14:05:10 +0100

    a

---
+++ app/Models/MainModel.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace App\Model;
+use Nette;
+
+class MainModel
+{
+
+    private $database;
+    public function __construct(Nette\Database\Context $database)
+    {
+        $this->database = $database;
+    }
+    /**
+     * Return list of all courses in database
+     * Exception on error
+     * @return void
+     */
+    public function getAllCourses()
+    {
+        $data = $this->database->table("course")->fetchAll();
+
+        if ($data) {
+            return $data;
+        } else {
+            return NULL;
+        }
+    }
+
+    /**
+     * Return list of all approved courses in database
+     * Exception on error
+     * @return void
+     */
+    public function getAllApprovedCourses()
+    {
+        $data = $this->database->table("course")->where("course_status != 0")->fetchAll();
+
+        if ($data) {
+            return $data;
+        } else {
+            return NULL;
+        }
+    }
+
+    /**
+     * Return list of all approved courses in database that name matches filter
+     * Return NULL when no result
+     * @param [string] $filter
+     * @return void
+     */
+    public function getAllCoursesByFilter(string $filter, string $search)
+    {
+        $query = "SELECT id_course, course_name, course_type, course_price FROM course WHERE (" . $filter . " LIKE '%" . $search . "%' AND course_status > 0)";
+
+        $data = $this->database->query($query)->fetchAll();
+
+        if ($data) {
+            return $data;
+        } else {
+            return NULL;
+        }
+    }
+
+    /**
+     * Return list of details about specified course 
+     * Exception on error
+     * @param integer $course_id
+     * @return void
+     */
+    public function getCourseDetails($course_id)
+    {
+        $data = $this->database->table("course")->where("id_course=?", $course_id)->fetch();
+
+        if ($data) {
+            return $data;
+        } else {
+            throw new \Exception("Error in SQL query");
+        }
+    }
+
+    /**
+     * Return full name of course guarantor
+     * Exception on error
+     * @param integer $id_guarantor
+     * @return string
+     */
+    public function getCourseGuarantorName($id_guarantor)
+    {
+        $data = $this->database->table("user")->where("id_user=?", $id_guarantor)->fetch();
+
+        if ($data) {
+            return $data->first_name . " " . $data->surname;
+        } else {
+            throw new \Exception("Error in SQL query");
+        }
+    }
+
+    /**
+     * Return list of courses, where is student registered
+     * Exception on error
+     * @param [type] $id_student
+     * @return void
+     */
+    public function getCoursesOfStudent($id_student)
+    {
+        $data = $this->database->query("SELECT id_course, course_name, course_type, course_price FROM user NATURAL JOIN course_has_student NATURAL JOIN course WHERE id_user = ? AND student_status = 1 AND course_status != 0",  $id_student);
+
+		if($data->getRowCount() > 0)
+		{
+			return $data;
+        }
+        else
+        {
+            return NULL;
+        }
+    }
+
+    /**
+     * Return true if course has open registration for students, otherwise false
+     * Return NULL when course doesnt exist
+     * @param [type] $course_id
+     * @return int
+     */
+    public function checkOpenRegistration($course_id)
+    {
+        $data=$this->database->table("course")->select("course_status")->where("id_course=?", $course_id)->fetch();
+        if($data)
+        {
+            if($data->course_status>=2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return NULL;
+        }
+    }
+    /**
+     * Return status of user in course
+     * Return NULL when no record found
+     * @param [type] $courseId
+     * @param [type] $userId
+     * @return int
+     */
+    public function checkStudentCourseStatus($courseId, $userId)
+    {
+        $data = $this->database->table("course_has_student")->select("student_status")->where("id_course=? AND id_user=?", $courseId, $userId )->fetch();
+        if($data)
+        {
+            return $data->student_status;
+        }
+        else
+        {
+            return NULL;
+        }
+    }
+
+}
--- app/Models/VisitorModel.php
+++ app/Models/VisitorModel.php
@@ -10,96 +10,11 @@ use Nette\Application\UI\Form;
 class VisitorModel
 {
 
-    private $database;
-    public function __construct(Nette\Database\Context $database)
+    private $database, $mainModel;
+    public function __construct(Nette\Database\Context $database, \App\Model\MainModel $mainModel)
     {
         $this->database = $database;
-    }
-
-
-    /**
-     * Return list of all courses in database
-     * Exception on error
-     * @return void
-     */
-    public function getAllCourses()
-    {
-        $data = $this->database->table("course")->fetchAll();
-
-        if ($data) {
-            return $data;
-        } else {
-            return NULL;
-        }
-    }
-
-    /**
-     * Return list of all approved courses in database
-     * Exception on error
-     * @return void
-     */
-    public function getAllApprovedCourses()
-    {
-        $data = $this->database->table("course")->where("course_status != 0")->fetchAll();
-
-        if ($data) {
-            return $data;
-        } else {
-            return NULL;
-        }
-    }
-
-    /**
-     * Return list of all approved courses in database that name matches filter
-     * Return NULL when no result
-     * @param [string] $filter
-     * @return void
-     */
-    public function getAllCoursesByFilter(string $filter, string $search)
-    {
-        $query = "SELECT id_course, course_name, course_type, course_price FROM course WHERE (" . $filter . " LIKE '%" . $search . "%' AND course_status > 0)";
-
-        $data = $this->database->query($query)->fetchAll();
-
-        if ($data) {
-            return $data;
-        } else {
-            return NULL;
-        }
-    }
-
-    /**
-     * Return list of details about specified course 
-     * Exception on error
-     * @param integer $course_id
-     * @return void
-     */
-    public function getCourseDetails($course_id)
-    {
-        $data = $this->database->table("course")->where("id_course=?", $course_id)->fetch();
-
-        if ($data) {
-            return $data;
-        } else {
-            throw new \Exception("Error in SQL query");
-        }
-    }
-
-    /**
-     * Return full name of course guarantor
-     * Exception on error
-     * @param integer $id_guarantor
-     * @return string
-     */
-    public function getCourseGuarantorName($id_guarantor)
-    {
-        $data = $this->database->table("user")->where("id_user=?", $id_guarantor)->fetch();
-
-        if ($data) {
-            return $data->first_name . " " . $data->surname;
-        } else {
-            throw new \Exception("Error in SQL query");
-        }
+        $this->mainModel = $mainModel;
     }
 
     /**
@@ -114,8 +29,8 @@ class VisitorModel
         if (empty($id)) {
             $presenter->redirect('Homepage:courses');
         }
-        $presenter->template->course = $this->getCourseDetails($id);
-        $presenter->template->guarantor = $this->getCourseGuarantorName($presenter->template->course->id_guarantor);
+        $presenter->template->course = $this->mainModel->getCourseDetails($id);
+        $presenter->template->guarantor = $this->mainModel->getCourseGuarantorName($presenter->template->course->id_guarantor);
 
         switch ($presenter->template->course->course_type) {
             case "P":
