40777cc8cd727cfb357648447d14df575a939b17
Parradox27 <40549021+Parradox27@users.noreply.github.com>
2019-11-28 20:06:09 +0100

    Update GarantPresenter.php

--- app/Presenters/GarantPresenter.php
+++ app/Presenters/GarantPresenter.php
@@ -270,7 +270,7 @@ final class GarantPresenter extends Nette\Application\UI\Presenter
 	public function createTaskForm(Nette\Application\UI\Form $form): void
     {
     	$values = $form->getValues();
-
+    	dump($values);
     	if($values->task_points == '') $values->task_points = NULL;
 
     	$result = $this->database->query("INSERT INTO task (id_task, task_name, task_type, task_description, task_points, task_date, task_from, task_to, id_room, id_course) VALUES ('',?,?,?,?,?,?,?,?,?)", $values->task_name, $values->task_type, $values->task_description, $values->task_points, $values->task_date, $values->task_from, $values->task_to, $values->id_room, $values->id_course);
