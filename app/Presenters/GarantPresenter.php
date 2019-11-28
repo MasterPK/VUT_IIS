53420a7ef0ebd54d095705aad927dcb55288ed6b
Parradox27 <40549021+Parradox27@users.noreply.github.com>
2019-11-28 20:16:04 +0100

    Update GarantPresenter.php

--- app/Presenters/GarantPresenter.php
+++ app/Presenters/GarantPresenter.php
@@ -273,7 +273,7 @@ final class GarantPresenter extends Nette\Application\UI\Presenter
     	dump($values);
     	if($values->task_points == '') $values->task_points = NULL;
 
-    	$result = $this->database->query("INSERT INTO task (id_task, task_name, task_type, task_description, task_points, task_date, task_from, task_to, id_course) VALUES ('',?,?,?,?,?,?,?,?,?)", $values->task_name, $values->task_type, $values->task_description, $values->task_points, $values->task_date, $values->task_from, $values->task_to, $values->id_course);
+    	$result = $this->database->query("INSERT INTO task (id_task, task_name, task_type, task_description, task_points, task_date, task_from, task_to, id_course) VALUES ('',?,?,?,?,?,?,?,?)", $values->task_name, $values->task_type, $values->task_description, $values->task_points, $values->task_date, $values->task_from, $values->task_to, $values->id_course);
 
     	if($values->id_room != NULL)
     	{
