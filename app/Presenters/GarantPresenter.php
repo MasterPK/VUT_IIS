dca117522373cfa8bb9927a10589aa886939264f
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-11-28 22:22:32 +0100

    Update GarantPresenter.php

--- app/Presenters/GarantPresenter.php
+++ app/Presenters/GarantPresenter.php
@@ -102,7 +102,7 @@ final class GarantPresenter extends Nette\Application\UI\Presenter
 
     	try
     	{
-    		$data = $this->database->query("INSERT INTO course (id_course, course_name, course_description, course_type, course_price, id_guarantor, course_status) VALUES (?, ?, ?, ?, ?, ?, 0)", $values->id_course, $values->name, $values->description, $values->type, $values->price,  $this->user->identity->id);
+    		$data = $this->database->query("INSERT INTO course (id_course, course_name, course_description, course_type, course_price, id_guarantor, course_status) VALUES (?, ?, ?, ?, ?, ?, 0,?)", $values->id_course, $values->name, $values->description, $values->type, $values->price, $values->tags,  $this->user->identity->id);
 
     		$this->template->success_insert = true;
     	}
