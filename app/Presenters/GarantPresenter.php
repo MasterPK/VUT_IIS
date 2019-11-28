4cf353f2360458daa4e39f80b26811d61d847149
Parradox27 <40549021+Parradox27@users.noreply.github.com>
2019-11-28 19:29:16 +0100

    Update GarantPresenter.php

--- app/Presenters/GarantPresenter.php
+++ app/Presenters/GarantPresenter.php
@@ -84,7 +84,7 @@ final class GarantPresenter extends Nette\Application\UI\Presenter
 			}
 		}
 		$rooms = $this->database->query("SELECT id_room FROM room")->fetchAll();
-		$category = [{NULL => "Žádná"}];
+		$category = [NULL => "Žádná"];
 		foreach($rooms as $room)
 		{
 			array_push($category, {$room->id_room => $room->id_room});
