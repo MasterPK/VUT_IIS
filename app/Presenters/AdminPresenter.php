8beceeb0953b29a5ed5e4908ac3090db814d2447
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-11-28 10:05:27 +0100

    a

---
+++ app/Presenters/AdminPresenter.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\UI\Form;
+use Ublaboo\DataGrid\DataGrid;
+
+
+class AdminPresenter extends Nette\Application\UI\Presenter 
+{
+	/** @var \App\Model\StartUp @inject */
+    public $startup;
+
+	/** @var Nette\Database\Context @inject */
+    public $database;
+    
+    /** @var \App\Model\ChiefModel @inject */
+	public $chiefModel;
+
+	/** @var \App\Model\GarantModel @inject */
+	public $garantModel;
+
+	/** @var \App\Model\StudentModel @inject */
+	public $studentModel;
+
+	/** @var \App\Model\MainModel @inject */
+	public $mainModel;
+
+
+	public function startUp()
+	{
+		parent::startup();
+
+		
+		$this->startup->mainStartUp($this);
+		if(!$this->startup->roleCheck($this,5))
+		{
+			$this->redirect("Homepage:default");
+		}
+    }
+
+    public function renderUserManagement()
+    {
+
+    }
+
+    public function createComponentUsersManagamentDataGrid()
+    {
+        $grid = new DataGrid($this, "Seznam uživatelů");
+
+		$grid->setDataSource($this->database->table("user"));
+        $grid->addColumnText('id_user', 'ID');
+        return $grid;
+    }
+}
