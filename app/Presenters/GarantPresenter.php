28fcb7bbdfb495e9e9cb2045206c34b6b650bf91
Parradox27 <40549021+Parradox27@users.noreply.github.com>
2019-11-28 17:54:17 +0100

    Update GarantPresenter.php

--- app/Presenters/GarantPresenter.php
+++ app/Presenters/GarantPresenter.php
@@ -207,6 +207,7 @@ final class GarantPresenter extends Nette\Application\UI\Presenter
         ->addRule(Form::MAX_LENGTH, 'Dĺžka popisu je maximálně 100 znaků!', 100);
 
         $form->addText('task_points', 'Počet bodů')
+        ->setDefaultValue(NULL);
         ->setHtmlAttribute('class', 'form-control')
         ->addRule(Form::RANGE, "Zadejte počet bodů v rozmezí 1 - 100!", [1,100]);
 
@@ -251,7 +252,7 @@ final class GarantPresenter extends Nette\Application\UI\Presenter
 	public function createTaskForm(Nette\Application\UI\Form $form): void
     {
     	$values = $form->getValues();
-    	if($values->task_points == 0) $values->task_points = NULL;
+    	//if($values->task_points == 0) $values->task_points = NULL;
     	$result = $this->database->query("INSERT INTO task (id_task, task_name, task_type, task_description, task_points, task_date, task_from, task_to, id_course) VALUES ('',?,?,?,?,?,?,?,?)", $values->task_name, $values->task_type, $values->task_description, $values->task_points, $values->task_date, $values->task_from, $values->task_to, $values->id_course);
     	if($result->getRowCount() > 0)
     	{
