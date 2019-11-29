78f6d588c2ec3b7b023f6d26a81f6f3eb0cb2b04
Martin Klobušický <martin.klobusicky@centrum.sl>
2019-11-29 11:39:59 +0100

    Update ChiefPresenter.php

--- app/Presenters/ChiefPresenter.php
+++ app/Presenters/ChiefPresenter.php
@@ -205,7 +205,7 @@ final class ChiefPresenter extends Nette\Application\UI\Presenter
             ->setHtmlAttribute('class', 'form-control')
 			->setRequired();
 			
-		$form->addSelect('room_Adres', '', $this->address)
+		$form->addSelect('room_Adres', '', $this->address['room_address'])
             ->setHtmlAttribute('class', 'form-control')
 			->setRequired();
 
