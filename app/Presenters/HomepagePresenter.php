32709cc2f51b7713b7cf775c0399a728626442f6
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-11-29 21:43:12 +0100

    Update HomepagePresenter.php

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -21,11 +21,33 @@ class HomepagePresenter extends Nette\Application\UI\Presenter
 	public $mainModel;
 
 
-
+	private $dataGridTranslator;
 	private $database;
 	public function __construct(Nette\Database\Context $database)
 	{
 		$this->database = $database;
+		$this->dataGridTranslator=new Ublaboo\DataGrid\Localization\SimpleTranslator([
+			'ublaboo_datagrid.no_item_found_reset' => 'Žádné položky nenalezeny. Filtr můžete vynulovat',
+			'ublaboo_datagrid.no_item_found' => 'Žádné položky nenalezeny.',
+			'ublaboo_datagrid.here' => 'zde',
+			'ublaboo_datagrid.items' => 'Položky',
+			'ublaboo_datagrid.all' => 'všechny',
+			'ublaboo_datagrid.from' => 'z',
+			'ublaboo_datagrid.reset_filter' => 'Resetovat filtr',
+			'ublaboo_datagrid.group_actions' => 'Hromadné akce',
+			'ublaboo_datagrid.show_all_columns' => 'Zobrazit všechny sloupce',
+			'ublaboo_datagrid.hide_column' => 'Skrýt sloupec',
+			'ublaboo_datagrid.action' => 'Akce',
+			'ublaboo_datagrid.previous' => 'Předchozí',
+			'ublaboo_datagrid.next' => 'Další',
+			'ublaboo_datagrid.choose' => 'Vyberte',
+			'ublaboo_datagrid.execute' => 'Provést',
+			'ublaboo_datagrid.per_page_submit'=>"Aktualizovat",
+
+			'Name' => 'Jméno',
+			'Inserted' => 'Vloženo'
+		]);
+
 	}
 
 	private $current_course_id;
@@ -47,8 +69,32 @@ class HomepagePresenter extends Nette\Application\UI\Presenter
 	{
 		$grid = new DataGrid($this, $name);
 		$grid->setPrimaryKey('id_course');
-		$grid->setDataSource($this->database->table('course'));
-		$grid->addColumnText('course_name', 'Jméno kurzu');
+		$grid->setDataSource($this->database->table('course'))
+		->setSortable()
+		->setFilterText();
+
+		$grid->addColumnText('course_name', 'Jméno kurzu')
+		->setSortable()
+		->setFilterText();
+
+		$grid->addColumnText('course_type', 'Typ kurzu')
+		->setSortable()
+		->setFilterText();
+
+		$grid->addColumnText('course_price', 'Cena kurzu')
+		->setSortable()
+		->setFilterText();
+
+		$grid->addColumnText('tags', 'Štítky')
+		->setSortable()
+		->setFilterText();
+
+		$grid->addAction("select","Detail", 'showcourse')
+		->setClass("btn btn-primary");
+
+		$grid->setTranslator($this->dataGridTranslator);
+
+		$grid->addColumnText('', '');
 		return $grid;
 	}
 
