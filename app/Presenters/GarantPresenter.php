d05fbd93eb1317cbf2bb5205bbfdbb4b40b9e754
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-11-29 11:41:56 +0100

    Update GarantPresenter.php

--- app/Presenters/GarantPresenter.php
+++ app/Presenters/GarantPresenter.php
@@ -333,7 +333,7 @@ final class GarantPresenter extends Nette\Application\UI\Presenter
     	else
     	{
     		$result = $this->database->query("INSERT INTO task (id_task, task_name, task_type, task_description, task_points, task_date, task_from, task_to, id_room, id_course) VALUES ('',?,?,?,?,?,?,?,?,?)", $values->task_name, $values->task_type, $values->task_description, $values->task_points, $values->task_date, $values->task_from, $values->task_to, $values->id_room, $values->id_course);
-			$task_id = $this->database->lastInsertId('task');
+			$task_id = $this->database->getInsertId('task');
 			Debugger::barDump($result);
 			Debugger::barDump($task_id,"id");
     		if($result->getRowCount() > 0)
