93c6ef8d23ea08ffdb81d77f52b30baf7c4cfc19
Martin Klobušický <martin.klobusicky@centrum.sl>
2019-11-29 21:03:56 +0100

    Update ChiefPresenter.php

--- app/Presenters/ChiefPresenter.php
+++ app/Presenters/ChiefPresenter.php
@@ -352,7 +352,7 @@ final class ChiefPresenter extends Nette\Application\UI\Presenter
 		$form->addSelect('room_Adres', '', $address)
 			->setHtmlAttribute('class', 'form-control')
 			->setRequired()
-			->setDefaultValue($address[$this->current_room["id_room_address"]]);
+			->setDefaultValue($address);
 
 		$form->addSubmit('submit', 'Upravit místnost')
 			->setHtmlAttribute('class', 'btn btn-block btn-primary ajax');
