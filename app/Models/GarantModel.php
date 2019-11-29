9386cd9b8d160b1cb1addbc9d337526bfb94d221
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-11-29 18:45:42 +0100

    Ajax na vytvoření kurzu

--- app/Models/GarantModel.php
+++ app/Models/GarantModel.php
@@ -83,7 +83,7 @@ class GarantModel
         ->setHtmlAttribute('class', 'form-control');
         
         $form->addSubmit('create', 'Vytvořit kurz')
-        ->setHtmlAttribute('class', 'btn btn-block btn-primary');
+        ->setHtmlAttribute('class', 'btn btn-block btn-primary ajax');
         
         $form->onSuccess[] = [$meno, 'createCourseForm'];
         return $form;
