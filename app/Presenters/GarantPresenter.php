eff911fc181c8dd0af3c3f6b304f148dd434fdcb
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-11-29 11:39:22 +0100

    Update GarantPresenter.php

--- app/Presenters/GarantPresenter.php
+++ app/Presenters/GarantPresenter.php
@@ -333,12 +333,12 @@ final class GarantPresenter extends Nette\Application\UI\Presenter
     	else
     	{
     		$result = $this->database->query("INSERT INTO task (id_task, task_name, task_type, task_description, task_points, task_date, task_from, task_to, id_room, id_course) VALUES ('',?,?,?,?,?,?,?,?,?)", $values->task_name, $values->task_type, $values->task_description, $values->task_points, $values->task_date, $values->task_from, $values->task_to, $values->id_room, $values->id_course);
-
+			$task_id = $this->database->lastInsertId('task');
     		if($result->getRowCount() > 0)
 	    	{
 				try
 				{
-					FileSystem::createDir("Files/$values->id_course/$values->id_task");
+					FileSystem::createDir("Files/$values->id_course/$task_id");
 					$this->template->create_task_success = 1;
 				}
 				catch(Nette\IOException $e)
