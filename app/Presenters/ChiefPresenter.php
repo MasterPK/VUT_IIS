3972a22f3890c5f16119826d1365a43452c2ef19
Martin Klobušický <martin.klobusicky@centrum.sl>
2019-11-29 14:19:01 +0100

    Update ChiefPresenter.php

--- app/Presenters/ChiefPresenter.php
+++ app/Presenters/ChiefPresenter.php
@@ -239,7 +239,7 @@ final class ChiefPresenter extends Nette\Application\UI\Presenter
 
 		$this->template->success_notify = true;
         if ($this->isAjax()) {
-            $this->redrawControl("notify");
+            $this->redrawControl("content_snippet");
         }
 	}
 	
