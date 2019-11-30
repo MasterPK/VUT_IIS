7d07f75a865cad2ae0b7ddc2539c84937ced9ffb
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-12-01 00:37:43 +0100

    Update StudentPresenter.php

--- app/Presenters/StudentPresenter.php
+++ app/Presenters/StudentPresenter.php
@@ -230,8 +230,8 @@ class StudentPresenter extends Nette\Application\UI\Presenter
 			array_push($tasks,[
 				"task_name"=>$value->task_name,
 				"day"=>$day,
-				"task_from"=>$value->task_from,
-				"task_to"=>$value->task_to
+				"task_from"=>$value->task_from==NULL?$value->task_to:$value->task_from,
+				"task_to"=>$value->task_from==NULL?$value->task_to+1:$value->task_to
 				]);
 			
 		}
