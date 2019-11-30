ff2c50fdc28edaacd46a29381ac4a9f4eae0820a
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-11-30 23:31:13 +0100

    Update StudentPresenter.php

--- app/Presenters/StudentPresenter.php
+++ app/Presenters/StudentPresenter.php
@@ -212,7 +212,7 @@ class StudentPresenter extends Nette\Application\UI\Presenter
 		$dayTasksCount=array();
 		foreach ($data as $value) {
 			setlocale(LC_TIME,"cs-CZ");
-			$day = date ('l',$value->task_date->getTimestamp());
+			$day = strftime ('%A',$value->task_date->getTimestamp());
 			Debugger::barDump($day,"den");
 		}
 		
