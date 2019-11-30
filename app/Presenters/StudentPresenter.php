f54b6d326ff92bd06d403c8703b24d9c743e9904
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-11-30 23:20:56 +0100

    Update StudentPresenter.php

--- app/Presenters/StudentPresenter.php
+++ app/Presenters/StudentPresenter.php
@@ -210,7 +210,7 @@ class StudentPresenter extends Nette\Application\UI\Presenter
 		$dayTasksCount=array();
 		foreach ($data as $value) {
 			
-			$day = strftime ('l',$value->task_date->getTimestamp());
+			$day = strftime ('%A',$value->task_date->getTimestamp());
 			Debugger:dump($day);
 		}
 		
