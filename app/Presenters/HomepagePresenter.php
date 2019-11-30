951b4f492d313656c126185df0a9a2d5ace6056c
Parradox27 <40549021+Parradox27@users.noreply.github.com>
2019-11-30 23:20:18 +0100

    added datagrid for task points

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -22,35 +22,11 @@ class HomepagePresenter extends Nette\Application\UI\Presenter
 	/** @var \App\Model\MainModel @inject */
 	public $mainModel;
 
+	/** @var \App\Model\DataGridModel @inject */
+	public $dataGridModel;
 
-	private $dataGridTranslator;
-	private $database;
-	public function __construct(Nette\Database\Context $database)
-	{
-		$this->database = $database;
-		$this->dataGridTranslator=new Ublaboo\DataGrid\Localization\SimpleTranslator([
-			'ublaboo_datagrid.no_item_found_reset' => 'Žádné položky nenalezeny. Filtr můžete vynulovat',
-			'ublaboo_datagrid.no_item_found' => 'Žádné položky nenalezeny.',
-			'ublaboo_datagrid.here' => 'zde',
-			'ublaboo_datagrid.items' => 'Položky',
-			'ublaboo_datagrid.all' => 'všechny',
-			'ublaboo_datagrid.from' => 'z',
-			'ublaboo_datagrid.reset_filter' => 'Resetovat filtr',
-			'ublaboo_datagrid.group_actions' => 'Hromadné akce',
-			'ublaboo_datagrid.show_all_columns' => 'Zobrazit všechny sloupce',
-			'ublaboo_datagrid.hide_column' => 'Skrýt sloupec',
-			'ublaboo_datagrid.action' => 'Akce',
-			'ublaboo_datagrid.previous' => 'Předchozí',
-			'ublaboo_datagrid.next' => 'Další',
-			'ublaboo_datagrid.choose' => 'Vyberte',
-			'ublaboo_datagrid.execute' => 'Provést',
-			'ublaboo_datagrid.per_page_submit'=>"Aktualizovat",
-
-			'Name' => 'Jméno',
-			'Inserted' => 'Vloženo'
-		]);
-
-	}
+	/** @var Nette\Database\Context @inject */
+	public $database;
 
 	private $current_course_id;
 
