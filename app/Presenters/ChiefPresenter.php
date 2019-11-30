76d7c80d0abb62993bdae9211662fd4b92b32927
Martin Klobušický <martin.klobusicky@centrum.sl>
2019-11-30 15:05:24 +0100

    Update ChiefPresenter.php

--- app/Presenters/ChiefPresenter.php
+++ app/Presenters/ChiefPresenter.php
@@ -185,7 +185,7 @@ final class ChiefPresenter extends Nette\Application\UI\Presenter
 
 	public function renderRoomsEquipment($id_room)
 	{
-		$data = $this->database->query("SELECT * FROM room_equipment NATURAL JOIN room_equipment WHERE id_room = ?",  $id_room)->fetchAll();
+		$data = $this->database->query("SELECT * FROM room NATURAL JOIN room_equipment WHERE id_room = ?",  $id_room)->fetchAll();
 
 		$this->template->equip = $data;
 		$this->template->id = $id_room;
