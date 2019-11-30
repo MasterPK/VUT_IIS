fb5a2c8749376e024a1c79a154fa62129fd2e9a1
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-11-30 10:52:35 +0100

    Update AdminPresenter.php

--- app/Presenters/AdminPresenter.php
+++ app/Presenters/AdminPresenter.php
@@ -397,7 +397,7 @@ class AdminPresenter extends Nette\Application\UI\Presenter
             ->setIcon('trash')
             ->setTitle('Smazat')
             ->setClass('btn btn-xs btn-danger ajax')
-            ->setConfirm('Opravdu chcet smazat uživatele %s?', 'email');
+            ->setConfirmation(new \Ublaboo\DataGrid\Column\Action\Confirmation\StringConfirmation('Opravdu chcet smazat uživatele %s?', 'email'));
 
 
         $grid->setTranslator($this->dataGridTranslator);
