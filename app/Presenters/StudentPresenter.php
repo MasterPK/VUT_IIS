f7b8c3f95899538372c4c706333203d1592b9d5e
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-11-30 16:26:57 +0100

    a

--- app/Presenters/StudentPresenter.php
+++ app/Presenters/StudentPresenter.php
@@ -50,10 +50,51 @@ class StudentPresenter extends Nette\Application\UI\Presenter
 
 	public function renderMycourses(): void
 	{
-		$this->template->courses=$this->mainModel->getCoursesOfStudent($this->user->identity->id);	
+		
 	}
 
 	public function createComponentCourses($name)
+	{
+		$grid = new DataGrid($this, $name);
+		$grid->setPrimaryKey('id_course');
+		$grid->setDataSource($this->database->query("SELECT id_course, course_name, course_type, course_price FROM user NATURAL JOIN course_has_student NATURAL JOIN course WHERE id_user = ? AND student_status = 1 AND course_status != 0",  $this->user->identity->id)->fetchAll());
+
+		$grid->addColumnText('id_course', 'Zkratka kurzu')
+		->setSortable()
+		->setFilterText();
+
+		$grid->addColumnText('course_name', 'Jméno kurzu')
+		->setSortable()
+		->setFilterText();
+		
+
+		$grid->addColumnText('course_type', 'Typ kurzu')
+		->setReplacement([
+			'P' => 'Povinný',
+			'V' => 'Volitelný'
+		])
+		->setSortable();
+
+		$grid->addFilterSelect('course_type', 'Typ kurzu:', ["P" => 'Povinný', "V" => 'Volitelný']);
+		
+		$grid->addColumnText('course_price', 'Cena kurzu')
+		->setSortable()
+		->setFilterText();
+
+		$grid->addColumnText('tags', 'Štítky')
+		->setSortable()
+		->setFilterText();
+
+		$grid->addAction("select","Detail", 'Student:showcourse')
+		->setClass("btn btn-primary");
+
+		$grid->setTranslator($this->dataGridModel->dataGridTranslator);
+
+	
+		return $grid;
+	}
+
+	public function createComponentMyCourses($name)
 	{
 		$grid = new DataGrid($this, $name);
 		$grid->setPrimaryKey('id_course');
