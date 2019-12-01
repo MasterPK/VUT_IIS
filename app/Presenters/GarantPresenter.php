3841d4dc6c7f0700df863d4e4e2ca3763fd7fd63
Parradox27 <40549021+Parradox27@users.noreply.github.com>
2019-12-01 11:16:15 +0100

    Update GarantPresenter.php

--- app/Presenters/GarantPresenter.php
+++ app/Presenters/GarantPresenter.php
@@ -833,7 +833,7 @@ final class GarantPresenter extends Nette\Application\UI\Presenter
 
 		$grid->addColumnText('points', 'Body')
 		->setSortable()
-		->setEditableCallback([$this, $this->id_task, 'handleAddPoints']);
+		->setEditableCallback('addPoints!');
 	
 		$grid->setTranslator($this->dataGridModel->dataGridTranslator);
 
