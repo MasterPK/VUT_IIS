e7a4f417335f6cd2901f51ed8e7edad83ac034dd
Parradox27 <40549021+Parradox27@users.noreply.github.com>
2019-12-01 19:43:14 +0100

    Update GarantPresenter.php

--- app/Presenters/GarantPresenter.php
+++ app/Presenters/GarantPresenter.php
@@ -846,17 +846,8 @@ final class GarantPresenter extends Nette\Application\UI\Presenter
 		
 		$grid->addColumnText('points', 'Body')
 		->setSortable()
-		->setEditableCallback(function($id, $value) {
-			$httpRequest = $this->getHttpRequest();
-			$id_task = $httpRequest->getQuery('id_task');
-			$maxpoints = $this->database->query("SELECT task_points FROM task WHERE id_task = ?", $id_task)->fetch();
-			if($maxpoints->task_points >= $value)
-			{
-				$this->database->query("UPDATE student_has_task SET points = ? WHERE id_user = ? AND id_task = ?", $value, $id, $id_task);
-			}
-		});
+		->setFilterText();
 		
-		/*
 		$grid->addInlineEdit()
             ->onControlAdd[] = function (Nette\Forms\Container $container): void {
             $container->addText('points', '');
@@ -877,7 +868,7 @@ final class GarantPresenter extends Nette\Application\UI\Presenter
 			{
 				$this->database->query("UPDATE student_has_task SET points = ? WHERE id_user = ? AND id_task = ?", $values->points, $id, $id_task);
 			}
-        };*/
+        };
 
 		$grid->setTranslator($this->dataGridModel->dataGridTranslator);
 
