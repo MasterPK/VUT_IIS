576634b272539651cb52fcc7ba073c56599d4c06
Parradox27 <40549021+Parradox27@users.noreply.github.com>
2019-12-01 11:09:03 +0100

    Update GarantPresenter.php

--- app/Presenters/GarantPresenter.php
+++ app/Presenters/GarantPresenter.php
@@ -840,9 +840,9 @@ final class GarantPresenter extends Nette\Application\UI\Presenter
 		return $grid;
 	}
 
-	public function handleAddPoints($id_user, $id_task, $points)
+	public function handleAddPoints($value, $value2)
 	{
-		\Tracy\Debugger::barDump($id_user, $id_task, $points);
+		\Tracy\Debugger::barDump($value, $value2);
 		\Tracy\Debugger::barDump($_POST);
 		//$this->database->query("UPDATE student_has_task SET points = ? WHERE id_user = ? AND id_task = ?", $points, $id_user, $id_task);
 	}
