2b2d715448200546dff1fe28918079cc4d54babe
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-12-01 17:00:39 +0100

    Update GarantPresenter.php

--- app/Presenters/GarantPresenter.php
+++ app/Presenters/GarantPresenter.php
@@ -843,7 +843,7 @@ final class GarantPresenter extends Nette\Application\UI\Presenter
 			{
 				$this->database->query("UPDATE student_has_task SET points = ? WHERE id_user = ? AND id_task = ?", $value, $id, $id_task);
 			}
-			
+			return $value;
 		});
 		
 		/*$grid->addInlineEdit()
