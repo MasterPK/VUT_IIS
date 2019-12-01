393a46b6d68af3a71ee7e4a54d4de18af7b29979
Parradox27 <40549021+Parradox27@users.noreply.github.com>
2019-12-01 19:33:28 +0100

    Update GarantPresenter.php

--- app/Presenters/GarantPresenter.php
+++ app/Presenters/GarantPresenter.php
@@ -830,7 +830,7 @@ final class GarantPresenter extends Nette\Application\UI\Presenter
 		$grid = new DataGrid($this, $name);
 		$grid->setPrimaryKey('id_user');
 		//$grid->setDataSource($this->database->query("SELECT id_user, email, first_name, surname, points FROM user NATURAL JOIN student_has_task"));
-		$grid->setDataSource($this->database->table("student_has_task")->where("id_task = ?", $id_task)->select("user.id_user,user.email,user.first_name,user.surname,points"));
+		$grid->setDataSource($this->database->table("student_has_task")->where("student_has_task.id_task = ?", $id_task)->select("user.id_user,user.email,user.first_name,user.surname,student_has_task.points"));
 
 		$grid->addColumnText('email', 'Email studenta')
 		->setSortable()
