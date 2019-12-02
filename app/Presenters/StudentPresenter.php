07a0eac6b747bbd7f23e1c21a39faf89fdcdde13
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2019-12-02 17:15:36 +0100

    Update StudentPresenter.php

--- app/Presenters/StudentPresenter.php
+++ app/Presenters/StudentPresenter.php
@@ -254,7 +254,7 @@ class StudentPresenter extends Nette\Application\UI\Presenter
 	public function renderTimetable()
 	{
 		//Get all tasks in student courses
-		$data = $this->database->query("SELECT task.* FROM course NATURAL JOIN course_has_student NATURAL JOIN task WHERE id_user=?;", $this->user->identity->id)->fetchAll();
+		$data = $this->database->query("SELECT * FROM course NATURAL JOIN course_has_student NATURAL JOIN task WHERE id_user=? AND course_status=1;", $this->user->identity->id)->fetchAll();
 		Debugger::barDump($data,"data");
 		if (!$data) {
 			return;
